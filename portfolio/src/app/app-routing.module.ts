b1035b92ee5b21895ee7bbdea113328fbb8cc526
ramiJoudi <rami.joudi1@esprit.tn>
2021-10-10 12:24:32 +0100

    Home page

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 
+
+
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyBiiMibv9t4yonIy2_1l9_lHx121T6V9io",
+  authDomain: "rami-joudi.firebaseapp.com",
+  projectId: "rami-joudi",
+  storageBucket: "rami-joudi.appspot.com",
+  messagingSenderId: "48855817350",
+  appId: "1:48855817350:web:bdf3b23062366022cf4507",
+  measurementId: "G-71TVTTYRRN"
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,   NgxPageScrollModule,NgxPageScrollCoreModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
