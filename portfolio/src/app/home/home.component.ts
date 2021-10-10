34dc54e27607ad5a8afff12231bf030fd322ea3f
ramiJoudi <rami.joudi1@esprit.tn>
2021-10-10 14:55:57 +0100

    skills chart

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -6,12 +6,18 @@ declare var anchor: any;
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
-
+ 
   constructor() { }
-
+   s=true;
   ngOnInit(): void {
-    
+   
+     
+  }
+
+  show(){
+    this.s=false;
      
   }
 
